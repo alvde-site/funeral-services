d9904ff367398ee21741c17643ad078fad2d947e
Aleksandr Demidenko <sashatro@mail.ru>
2022-06-30 10:16:38 +0300

    feat:Добавить выпадающие ссылки

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,11 @@
 const burger = document.querySelector('.burger-menu');
+const navContent = document.querySelector('.topnav__content');
+const menu = document.querySelector('.topnav__links');
 
 export const toggleBurger = () => {
   burger.addEventListener('click',()=>{
     burger.classList.toggle('open');
+    menu.classList.toggle('topnav__links_opened');
+    navContent.classList.toggle('topnav__content_opened');
   })
 }
